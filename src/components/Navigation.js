f842c36f11db9daf4b921ae61ba36affd4670290
Odukoya <olatunjicharlesodus@gmail.com>
2021-02-09 03:45:22 -0800

    edit naviagtion

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
-import {Link } from 'react-router-dom';
 import './Navigation.css'
 
 
